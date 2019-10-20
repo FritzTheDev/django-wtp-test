faa68ae00a324a3f41905d759db69ec9f17f39b4
Frederick Johnson <fritzj96@gmail.com>
2019-10-19 18:24:06 -0700

    added readability-whitespace

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,7 @@ class CustomUserCreationForm(UserCreationForm):
   class Meta:
     model = CustomUser
     fields = ('username', 'email')
+    
 class CustomUserChangeForm(UserChangeForm):
   class Meta:
     model = CustomUser
